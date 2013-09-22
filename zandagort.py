bda41e94c9d834ffd5b95900ee92fa9362789151
Cucu <cucu.szpem@gmail.com>
2013-09-22 03:32:49 +0200

    Server pylinted a bit

--- zandagort.py
+++ zandagort.py
@@ -7,7 +7,6 @@ python server.py
 
 import errno
 import json
-import sys
 import datetime
 import Cookie
 from urlparse import urlparse, parse_qs
@@ -24,15 +23,27 @@ from myenum import MyEnum
 
 
 class InnerCommands(MyEnum):
+    """Inner commands for ZandagortServer"""
 
     values = ["Sim", "Dump"]
 
 
+def _parse_qs_flat(query):
+    """Return flat version of parse_qs. 'q=a,b' becomes "q":"a,b" not "q":["a","b"]"""
+    deep_query_dict = parse_qs(query)
+    flat_query_dict = {}
+    for key in deep_query_dict:
+        flat_query_dict[key] = deep_query_dict[key][0]
+    return flat_query_dict
+
+
 class ZandagortRequestHandler(BaseHTTPRequestHandler):
+    """HTTP request handler for ZandagortHTTPServer"""
     
     server_version = config.ZANDAGORT_SERVER_VERSION
     
     def do_GET(self):
+        """Handle GET requests"""
         url = urlparse(self.path)
         command = url.path.lstrip("/")
         if command == "test" or command == "test/":
@@ -45,13 +56,14 @@ class ZandagortRequestHandler(BaseHTTPRequestHandler):
             self._send_static_file("img/favicon.ico", "image/x-icon")
             return
         try:
-            argument = json.dumps(self._parse_qs_flat(url.query))
+            argument = json.dumps(_parse_qs_flat(url.query))
         except Exception:
             argument = '{"error": "syntax_error"}'
         response = self._get_response("GET", command, argument)
         self._send_response(response)
     
     def do_POST(self):
+        """Handle POST requests"""
         command = self.path.lstrip("/")
         try:
             request_body_length = int(self.headers.getheader("content-length"))
@@ -64,12 +76,14 @@ class ZandagortRequestHandler(BaseHTTPRequestHandler):
         response = self._get_response("POST", command, argument)
         self._send_response(response)
     
-    def log_message(self, format, *args):
+    def log_message(self, format_, *args):
+        """Overwrite (disable) default logging"""
         pass
     
     def _get_response(self, method, command, argument):
+        """Get response from core Zandagort Server"""
         my_queue = Queue.Queue()
-        self.server._request_queue.put({
+        self.server.request_queue.put({
             "response_queue": my_queue,
             "method": method,
             "command": command,
@@ -80,6 +94,7 @@ class ZandagortRequestHandler(BaseHTTPRequestHandler):
         return response
     
     def _send_response(self, response, content_type="application/json"):
+        """Send response to client"""
         self.send_response(200)
         self.send_header("Content-type", content_type + "; charset=utf-8")
         self.send_header("Content-length", str(len(response)))
@@ -90,10 +105,10 @@ class ZandagortRequestHandler(BaseHTTPRequestHandler):
                 auth_cookie_value = cookies[config.AUTH_COOKIE_NAME].value
         if auth_cookie_value.startswith("stuff"):  # TODO: replace test code with real
             try:
-                x = int(auth_cookie_value[5:])
+                counter = int(auth_cookie_value[5:])
             except ValueError:
-                x = 0
-            auth_cookie_value = "stuff" + str(x+1)
+                counter = 0
+            auth_cookie_value = "stuff" + str(counter+1)
         else:
             auth_cookie_value = "stuff"
         self._send_cookie(config.AUTH_COOKIE_NAME, auth_cookie_value, config.AUTH_COOKIE_EXPIRY, "/")
@@ -101,30 +116,25 @@ class ZandagortRequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(response)
     
     def _send_cookie(self, cookie_key, cookie_value, expires_from_now, path):
-        C = Cookie.SimpleCookie()
-        C[cookie_key] = cookie_value
+        """Send cookie with key, value, expiry and path"""
+        cookie = Cookie.SimpleCookie()
+        cookie[cookie_key] = cookie_value
         expires = datetime.datetime.now() + datetime.timedelta(seconds=expires_from_now)
-        C[cookie_key]["expires"] = expires.strftime("%a, %d-%b-%Y %H:%M:%S GMT")
-        C[cookie_key]["path"] = path
-        self.send_header("Set-Cookie", C.output(header=""))
-    
-    def _parse_qs_flat(self, query):
-        deep_query_dict = parse_qs(query)
-        flat_query_dict = {}
-        for key in deep_query_dict:
-            flat_query_dict[key] = deep_query_dict[key][0]
-        return flat_query_dict
+        cookie[cookie_key]["expires"] = expires.strftime("%a, %d-%b-%Y %H:%M:%S GMT")
+        cookie[cookie_key]["path"] = path
+        self.send_header("Set-Cookie", cookie.output(header=""))
     
     def _send_static_file(self, filename, content_type=None):
+        """Send static file to client"""
         content = ""
         if content_type is None:
             try:
-                type = filename[:filename.index("/")]
+                file_type = filename[:filename.index("/")]
             except ValueError:
-                type = "?"
-            if type == "js":
+                file_type = "?"
+            if file_type == "js":
                 content_type = "text/javascript"
-            elif type == "css":
+            elif file_type == "css":
                 content_type = "text/css"
             else:
                 content_type = "text/html"
@@ -134,14 +144,16 @@ class ZandagortRequestHandler(BaseHTTPRequestHandler):
 
 
 class ZandagortHTTPServer(ThreadingMixIn, HTTPServer):
+    """Multi threaded HTTP server between clients and ZandagortServer"""
     
     def __init__(self, server_address, request_queue):
         HTTPServer.__init__(self, server_address, ZandagortRequestHandler)  # can't use standard super() because HTTPServer is old-style class
-        self._request_queue = request_queue
+        self.request_queue = request_queue
         self.daemon_threads = True
 
 
 class ZandagortServer(object):
+    """Single threaded core server to handle Game"""
     
     def __init__(self, host, port):
         self._host = host
@@ -156,11 +168,13 @@ class ZandagortServer(object):
         self._game = Game(10000)
     
     def start(self):
+        """Start server and cron threads"""
         self._server_thread.start()
         self._cron.start()
         print "Listening at " + self._host + ":" + str(self._port) + "..."
     
     def serve_forever(self):
+        """Main loop of core server"""
         try:
             while True:
                 try:
@@ -181,6 +195,7 @@ class ZandagortServer(object):
             self._server.shutdown()
     
     def _execute_inner_command(self, command):
+        """Execute inner commands like Sim or Dump"""
         if command == InnerCommands.Sim:
             self._game.sim()
             print "[" + str(command) + "] game time =", self._game.get_time()
@@ -192,6 +207,7 @@ class ZandagortServer(object):
             print "[" + str(command) + "] Unknown command"
     
     def _execute_client_request(self, method, command, argument):
+        """Execute commands sent by clients"""
         print "[" + method + "]", command
         print "<argument>"
         print argument
@@ -203,12 +219,15 @@ class ZandagortServer(object):
         return response
     
     def _cron_fun(self, command):
+        """Simple helper function for cron thread"""
         self._request_queue.put({
             "inner_command": command
         })
 
 
 def main():
+    """Create, start and run Zandagort Server"""
+    
     print "Launching Zandagort Server..."
     try:
         server = ZandagortServer(config.ZANDAGORT_SERVER_HOST, config.ZANDAGORT_SERVER_PORT)
